b607ed7c90ea9741fe68224be868e95b3faf072a
hans90 <hasankaval77@gmail.com>
2020-08-22 01:24:56 +0200

    Changed condition ->Name

--- Assignment - 1 (if-Statements).py
+++ Assignment - 1 (if-Statements).py
@@ -1,5 +1,5 @@
 name = (input("Please enter your name  :   ")).title().strip()
-if name == "Joseph":
+if name == "Hasan":
     print(f"Hello, {name}! The password is : W@12")
 else:
     print("Hello, {}! See you later.".format(name))
